d0f8f2076ec817470bed0bc9a2bff57d411da8a4
yogeshmasaistudent <yogeshkushawah93@gmial.com>
2024-02-22 16:18:33 +0530

    done

--- TMetric/src/pages/Homepage/Homepage.jsx
+++ TMetric/src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,27 @@
 
-import Navbar from './Navbar'
+// import Navbar from './Navbar'
+
+import Firstsection from "../component/Firstsection"
+import { ManageTeamData } from "./ManageTeamData"
+// import MakeTime from "./MakeTime"
+import { PopularServices } from "./PopularService"
+import { SingleTab } from "./SingalTab"
+import { TrustedByCompany } from "./TrustByCompany"
+import { WhyTmetric } from "./WhyTemtric"
 
 
 function Homepage() {
   return (
-    <div>Homepage
-      <Navbar/>
-      {/* <Firstsection/> */}
+    <div>
+       <Firstsection/>
+       <ManageTeamData/>
+       <PopularServices/>
+       {/* <SingleTab/> */}
+       <TrustedByCompany/>
+       <WhyTmetric/>
+       {/* <MakeTime/> */}
+      
+
     </div>
   )
 }
