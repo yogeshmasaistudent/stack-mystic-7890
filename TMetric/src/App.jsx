3afc75534b2307b81fc3fc5b740cc192cd701199
Deepak Wayzode <dwayzode@gmail.com>
2024-02-22 11:58:35 +0530

    third fourth and four section is updated from homepage

--- TMetric/src/App.jsx
+++ TMetric/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Firstsection from './pages/component/Firstsection'
+import Secondsection from './pages/component/Secondsection'
+import Thirdsection from './pages/component/Thirdsection'
 
 function App() {
   const [count, setCount] = useState(0)
--- TMetric/src/main.jsx
+++ TMetric/src/main.jsx
@@ -6,8 +6,8 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-   <ChakraProvider>
+
     <App />
-    </ChakraProvider>
+    
 
 )
