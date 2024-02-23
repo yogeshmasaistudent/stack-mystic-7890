90feb951a485c8f6b751da388a6c5320a4184114
Deepak Wayzode <dwayzode@gmail.com>
2024-02-23 09:25:18 +0530

    intial comment

---
+++ TMetric/src/routes/Allroutes.jsx
@@ -0,0 +1,36 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { HomePage } from "../pages/HomePage";
+import Support from "../pages/Support";
+
+
+import Login from "../pages/Login";
+import SignUp from "../pages/Signup";
+import Blog from "../pages/Blog";
+import Price from "../pages/Price";
+import { Navbar } from "../Components/Navbar";
+import { DropdownWhyTMatric } from "../Components/DropdownWhyTMatric";
+import { DropdownApp } from "../Components/DropdownAppIntegeration";
+
+const AllRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/why-tmetric" element={<DropdownWhyTMatric />} />
+        <Route path="/apps-integrations" element={<DropdownApp />} />
+
+        <Route path="/support" element={<Support />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/price" element={<Price />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AllRoutes;
