c19465bf45226a4790fda7e390491d48380ecf8c
abhishekhzb12 <abhishek.kumarhzb9@gmail.com>
2024-02-25 12:20:14 +0530

    added tsk

--- TMetric/src/App.jsx
+++ TMetric/src/App.jsx
@@ -2,12 +2,18 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AllRoutes from './routes/Allroutes'
+import Dashboard from './pages/Dashboard/Dashboard';
+import Task from './pages/Task/Task';
+import Time from './pages/Time/Time';
 
 function App() {
   return (
    <div>
-   <AllRoutes/>
+   {/* <AllRoutes/> */}
     {/* <Homepage/> */}
+    {/* <Dashboard/> */}
+    <Task/>
+    {/* <Time/> */}
    </div>
   )
 }
